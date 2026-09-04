c98e2eff19405e045574b00c5bce047b9734aaf8
touch <touchtech.tr@gmail.com>
2026-09-04 19:21:36 +0300

    fix: supabase client env guvenli hale getirildi

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+  const supabaseUrl = process.env.nbdztaphtyiwjyglbrpz || ''
+  const supabaseAnonKey = process.env.sb_publishable_PahwTbUG9dcTSBX50IubDA_1aPWoU5I || ''
 
   return createBrowserClient(supabaseUrl, supabaseAnonKey)
 }
