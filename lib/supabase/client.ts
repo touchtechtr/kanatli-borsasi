54fcdf23fc79cb6b23b21019d20ddf000f27926f
touch <touchtech.tr@gmail.com>
2026-09-04 19:48:00 +0300

    fix: env isimleri duzeltildi

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  const supabaseUrl = process.env.nbdztaphtyiwjyglbrpz || ''
+  const supabaseUrl = process.[https://nbdztaphtyiwjyglbrpz.supabase.co](https://nbdztaphtyiwjyglbrpz.supabase.co) || ''
   const supabaseAnonKey = process.env.sb_publishable_PahwTbUG9dcTSBX50IubDA_1aPWoU5I || ''
 
   return createBrowserClient(supabaseUrl, supabaseAnonKey)
