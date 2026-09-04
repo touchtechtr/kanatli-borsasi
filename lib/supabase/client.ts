de449cf4810ee96de702dd30464dcc630a7c23cb
touch <touchtech.tr@gmail.com>
2026-09-05 00:17:41 +0300

    fix: supabase client env variables

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,8 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  const supabaseUrl = process.[https://nbdztaphtyiwjyglbrpz.supabase.co](https://nbdztaphtyiwjyglbrpz.supabase.co) || ''
-  const supabaseAnonKey = process.env.sb_publishable_PahwTbUG9dcTSBX50IubDA_1aPWoU5I || ''
-
-  return createBrowserClient(supabaseUrl, supabaseAnonKey)
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
 }
